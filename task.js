b4e5df698f8e7665b830d75ca388cca7e2b9b475
WasilB <muhammadwasil23703@gmail.com>
2022-11-15 18:48:36 +0500

    loop through blocks

--- task.js
+++ task.js
@@ -9,35 +9,40 @@ const CONTRACT_ABI = require("./abi");
 const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
 
 async function getEvents() {
-  let latest_block = await web3.eth.getBlockNumber();
-  let historical_block = latest_block; // you can also change the value to 'latest' to get the latest block
+  let latest_block = await web3.eth.getBlockNumber(); //gets the latest block number
+  let historical_block = 10000000; // you can also change the value to 'latest' to get the latest block
   console.log("latest: ", latest_block, "historical block: ", historical_block);
-  const events = await contract.getPastEvents(
-    "Transfer", // change if your looking for a different event
-    { fromBlock: historical_block, toBlock: latest_block }
-  );
 
-  await getTransferDetails(events);
+  // loop through the blocks
+  for (let i = historical_block; i <= latest_block; i++) {
+    const events = await contract.getPastEvents(
+      "Transfer", // change if your looking for a different event
+      { fromBlock: i, toBlock: i }
+    );
+
+    await getTransferDetails(events);
+  }
 }
 
 async function getTransferDetails(data_events) {
   for (i = 0; i < data_events.length; i++) {
-    let from = data_events[i]["returnValues"]["from"];
+    let from = data_events[i]["returnValues"]["from"]; // gets the from address
     let to = data_events[i]["returnValues"]["to"];
     let amount = await data_events[i]["returnValues"]["value"];
 
-    let converted_amount = web3.utils.fromWei(amount, "ether");
+    let converted_amount = web3.utils.fromWei(amount, "ether"); //converts the amount to ether
 
     console.log("From:", from, "- To:", to, "- Value:", converted_amount);
   }
 }
-var job = new CronJob(
-  "*/15 * * * * *",
-  function () {
-    getEvents();
-  },
-  null,
-  true,
-  "America/Los_Angeles"
-);
-job.start();
+getEvents();
+// var job = new CronJob(
+//   "*/59 * * * * *",
+//   function () {
+//     getEvents();
+//   },
+//   null,
+//   true,
+//   "America/Los_Angeles"
+// );
+// job.start();
